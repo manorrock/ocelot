023ce5112015597138e94eb9e4a8dcd0e62d3e26
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-04-19 10:20:07 -0600

    Fixes issue #66 - Remove webapp

--- webapp/src/main/java/com/manorrock/ocelot/FacesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.manorrock.ocelot;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.faces.annotation.FacesConfig;
-
-@FacesConfig
-@ApplicationScoped
-public class FacesConfiguration {
-}
