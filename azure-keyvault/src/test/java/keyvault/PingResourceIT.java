32ab28245c50fb9d3be00eb6afda29db4ab32a33
Manfred Riem <mriem@manorrock.com>
2023-11-30 17:07:14 -0600

    Fixes #268 - Change Azure KeyVault container to be self-contained (#269)

---
+++ azure-keyvault/src/test/java/keyvault/PingResourceIT.java
@@ -0,0 +1,30 @@
+package keyvault;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The JUnit test for the PingResource class.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+class PingResourceIT {
+
+    /**
+     * Ping the resource.
+     * 
+     * @throws Exception when a serious error occurs.
+     */
+    @Test
+    void testPing() throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(new URI("http://localhost:8080/api/ping")).build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        assertEquals("OK", response.body());
+    }
+}
