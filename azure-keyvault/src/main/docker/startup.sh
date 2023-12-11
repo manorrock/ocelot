7bd02c86fde4d552881ce4ee0bb2a7b9332c59c8
Manfred Riem <mriem@manorrock.com>
2023-12-11 12:02:41 -0600

    Fixes #278 - Rename keystore.jks to keystore

--- azure-keyvault/src/main/docker/startup.sh
+++ azure-keyvault/src/main/docker/startup.sh
@@ -3,4 +3,4 @@
 #
 # Run the application
 #
-java -jar piranha-dist-webprofile.jar --war-file ROOT.war --https-port 8443 --https-keystore-file certs/keystore.jks --https-keystore-password password
+java -jar piranha-dist-webprofile.jar --war-file ROOT.war --https-port 8443 --https-keystore-file certs/keystore --https-keystore-password password
