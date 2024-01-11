9eb0da83b81b32a93b54a0c7c6948605bea7bf8d
Manfred Riem <mriem@manorrock.com>
2024-01-11 08:56:37 -0600

    Fixes #321 - Change Azure Key Vault simulator to use latest Tomcat

--- azure-keyvault/src/main/docker/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#
-# Run the application
-#
-java -jar piranha-dist-webprofile.jar --war-file ROOT.war --http-port 8100 --https-port 8200 --https-keystore-file certs/keystore --https-keystore-password password
