b56ca865756c89f77a342a87f5f98e889ca44761
Manfred Riem <mriem@manorrock.com>
2024-03-23 09:20:53 -0500

    Fixes #373 - Add Dockerfile for Azure Storage (#374)

---
+++ azure-storage/src/main/java/storage/PingResource.java
@@ -0,0 +1,34 @@
+package storage;
+
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+/**
+ * Ping resource.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+@Path("ping")
+@Singleton
+public class PingResource {
+
+    /**
+     * Constructor.
+     */
+    public PingResource() {
+    }
+
+    /**
+     * Ping.
+     * 
+     * @return "OK"
+     */
+    @Path(value = "")
+    @GET
+    @Produces(value = "text/plain")
+    public String ping() {
+        return "OK";
+    }
+}
---
+++ azure-storage/src/main/java/storage/StorageApplication.java
@@ -0,0 +1,13 @@
+package storage;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * The Azure Storage application.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+@ApplicationPath("")
+public class StorageApplication extends Application {  
+}
